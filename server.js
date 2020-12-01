99c25baaae5bc91ddc51a3df5a16c18195efd461
Carlos Diaz <cdiaz86@gmail.com>
2020-12-01 15:29:38 -0800

    Moves handlers into MongoClient.

--- server.js
+++ server.js
@@ -6,24 +6,26 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const connectionString = process.env.DB_URI;
 
-app.listen(3000, function() {
-  console.log('listening on 3000');
-});
-
 MongoClient.connect(connectionString, { useUnifiedTopology: true }, (err, client) => {
+  const db = client.db('quotesapp');
+
   try {
+    app.listen(3000, function() {
+      console.log('listening on 3000');
+    });
+
     console.log('Connected to Database!');
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.get('/', (req, res) => {
+      res.sendFile(__dirname + '/index.html');
+    });
+
+    app.post('/quotes', (req, res) => {
+      console.log(req.body);
+    });
   } catch (error) {
     console.error(err);
   }
 })
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.post('/quotes', (req, res) => {
-  console.log(req.body);
-});
