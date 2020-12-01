35f6fe59495185b17931ebc6ba7a5c7038e22e80
Carlos Diaz <cdiaz86@gmail.com>
2020-12-01 15:11:51 -0800

    Connects to db.

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser= require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const connectionString = process.env.DB_URI;
 
 app.listen(3000, function() {
-  console.log('listening on 3000')
+  console.log('listening on 3000');
 });
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true }, (err, client) => {
+  try {
+    console.log('Connected to Database!');
+  } catch (error) {
+    console.error(err);
+  }
+})
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
