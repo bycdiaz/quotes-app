26951e2b5b3b18bf8d5a702f427ea443f60f12b3
Carlos Diaz <cdiaz86@gmail.com>
2020-12-01 20:39:41 -0800

    Adds markup and JS to delete vadar quote.

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 const update = document.querySelector('#update-button')
+const deleteButton = document.querySelector('#delete-button');
 
 update.addEventListener('click', _ => {
   fetch('/quotes', {
@@ -8,5 +9,27 @@ update.addEventListener('click', _ => {
       name: 'Darth Vadar',
       quote: 'I find your lack of faith disturbing.'
     })
-  });
+  })
+  .then(res => {
+    if (res.ok) return res.json()
+  })
+  .then(response => {
+    console.log(response)
+  })
+})
+
+deleteButton.addEventListener('click', _ => {
+  fetch('/quotes', {
+    method: 'delete',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: 'Darth Vadar'
+    })
+  })
+    .then(res => {
+      if (res.ok) return res.json();
+    })
+    .then(data => {
+      window.location.reload();
+    })
 })
